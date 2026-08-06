eb93b6cd9dd81b36037b17b319ba69c172949586
edenWerke <edenw6292@gmail.com>
2026-08-06 12:38:07 +0300

    add

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,3 @@
-export {getpool,closePool} from './db/pool'
+export { getPool, closePool } from "./db/pool";
+export { AppError } from "./errors/AppError";
+export { errorHandler } from "./errors/errorHandler";
