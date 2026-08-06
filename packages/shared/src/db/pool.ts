a428f8ac9d8ed515a054feda3d36c736bf79a700
edenWerke <edenw6292@gmail.com>
2026-08-06 12:02:30 +0300

    pool

--- packages/shared/src/db/pool.ts
+++ packages/shared/src/db/pool.ts
@@ -6,5 +6,13 @@ let pool : Pool | null= null;
         if (!connectionString){
             throw new Error(' DATABASE_URL is not set')
         } 
+        pool=new Pool({connectionString})
     }
+    return pool
+ }
+ export async function closePool():Promise<void>{
+    if(pool){
+        await pool.end()
+    }
+
  }
