3013ac0295671c59b865ac93bf26a60d494a3dad
edenWerke <edenw6292@gmail.com>
2026-08-10 10:56:30 +0300

    validation

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,6 @@ export { getPool, closePool } from "./db/pool";
 export { AppError } from "./errors/AppError";
 export { errorHandler } from "./errors/errorHandler";
 
+export { logger } from "./logger/logger";
+export { httpLogger } from "./logger/httpLogger";
+export { successResponse, failResponse } from "./response/response"
--- packages/shared/src/logger/httpLogger.ts
+++ packages/shared/src/logger/httpLogger.ts
@@ -1,3 +1,5 @@
+// HTTP status code = tells the client what happened.
+// HTTP logger = tells you what happened.
 import { pinoHttp } from "pino-http";
 import { logger } from "./logger";
 
