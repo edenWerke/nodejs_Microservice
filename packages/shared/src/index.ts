24b9fc60f7f58e46e3a0d6ed3d210c5b94a02cd6
edenWerke <edenw6292@gmail.com>
2026-08-10 11:10:49 +0300

    auth service

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,4 +4,5 @@ export { errorHandler } from "./errors/errorHandler";
 
 export { logger } from "./logger/logger";
 export { httpLogger } from "./logger/httpLogger";
-export { successResponse, failResponse } from "./response/response"
+export { successResponse, failResponse } from "./response/response";
+export {validateBody} from './validation/validateBody'
