ee2ecfe815657ddc4fd1f9b83c2efa323f3dc268
edenWerke <edenw6292@gmail.com>
2026-08-14 14:22:21 +0300

    routes

--- apps/auth-service/src/index.ts
+++ apps/auth-service/src/index.ts
@@ -9,7 +9,7 @@ import {
 
   successResponse,
 } from "shared";
-
+import authRoutes from './routes/auth.route'
 
 config({ path: resolve(process.cwd(), ".env") });
 config({ path: resolve(process.cwd(), "../../.env") });
@@ -25,7 +25,7 @@ app.get("/health", (_req, res) => {
   successResponse(res, { service: "auth-service" });
 });
 
-
+app.use ('/auth',authRoutes)
 
 app.use((_req, _res, next) => {
   next(new AppError(404, "Route not found"));
